2ec5f7c097300a26d6e06257b9cbc9c8f69f9f98
maorRoz <andory@post.bgu.ac.il>
2020-04-25 04:35:39 +0300

    removed redundant files

--- src/types/ContainerProps.ts
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from 'react';
-
-export type ContainerProps = {
-  children?: ReactNode;
-};
--- src/types/DisplayProps.ts
+++
@@ -1,6 +0,0 @@
-import { CSSProperties } from 'react';
-
-export type DisplayProps = {
-  className?: string;
-  style?: CSSProperties;
-};
